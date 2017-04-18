93232f152dca58de7ec0342cefb942ad618a9412
Yasir <yasir_cse@hotmail.com>
2017-04-18 16:27:41 +0500

    Articles Blog App
    Articles Blog App

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './App.css'
+const App = () => {
+    return (
+        <div>
+            <h1 className={styles.heading}>Articles blog</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const {resolve} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const srcDir = resolve(__dirname,'src')
+
+module.exports={
+    entry:`${srcDir}/index.js`,
+    output:{
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[{
+            test:/\.js$/,
+            loader:'babel-loader',
+            exclude:'/node_modules/'
+        },{
+            test:/\.css$/,
+            use:[{
+                loader:'style-loader'
+            },{
+                loader:'css-loader',
+                options:{
+                    modules:true,
+                    localIdentName:'[name]-[local]-[hash:base64:6]',
+                    camelCase:true
+                }
+            }]
+        }]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:`${srcDir}/index.html`
+        })
+    ]
+}
